bdc0c961d7e3a705a847cfcd10b5a0cd94529ba9
Administrator <changhuan.huang@gmail.com>
2018-08-25 23:41:42 +0800

    熔断器

--- movie-server/src/main/java/com/yimi/product/movieserver/remote/UserRemote.java
+++ movie-server/src/main/java/com/yimi/product/movieserver/remote/UserRemote.java
@@ -8,12 +8,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name= "user-server")
+/**
+ * 远程调用服务
+ */
+@FeignClient(name= "user-server",fallback = UserRemoteHystrix.class)
 public interface UserRemote {
+    /**
+     *
+     * @return
+     */
     @RequestMapping(value = "/v001/user/searchAll")
     public String searchAll();
 
-
+    /**
+     *
+     * @return
+     */
     @RequestMapping(value = "/v001/user/searchAll")
     public ServiceResult<List<SysUser>> searchAll1();
 }
---
+++ movie-server/src/main/java/com/yimi/product/movieserver/remote/UserRemoteHystrix.java
@@ -0,0 +1,24 @@
+package com.yimi.product.movieserver.remote;
+
+import com.yimi.product.movieserver.dto.ServiceResult;
+import com.yimi.product.movieserver.entity.SysUser;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 熔断fallback
+ */
+@Component
+public class UserRemoteHystrix implements UserRemote{
+    @Override
+    public String searchAll() {
+        return "查询失败";
+    }
+
+    @Override
+    public ServiceResult<List<SysUser>> searchAll1() {
+        return new ServiceResult<List<SysUser>>(false,new ArrayList<>());
+    }
+}
