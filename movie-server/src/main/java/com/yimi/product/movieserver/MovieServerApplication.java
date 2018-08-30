5a112eec7b9acee29081c48cf51bba862ebf9cd7
peter <2464375449@qq.com>
2018-08-30 11:29:58 +0800

    整理

--- movie-server/src/main/java/com/yimi/product/movieserver/MovieServerApplication.java
+++ movie-server/src/main/java/com/yimi/product/movieserver/MovieServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
