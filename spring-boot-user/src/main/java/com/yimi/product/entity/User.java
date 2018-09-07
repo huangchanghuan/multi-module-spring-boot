781b65693a243486eccb8f7f40f99447bcacd250
Administrator <changhuan.huang@gmail.com>
2018-09-07 12:55:31 +0800

    契约测试

---
+++ spring-boot-user/src/main/java/com/yimi/product/entity/User.java
@@ -0,0 +1,13 @@
+package com.yimi.product.entity;
+
+public class User {
+    private String name="peter";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
